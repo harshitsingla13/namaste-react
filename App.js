561703d594f00da65f47e4cbcedc24e5bc2fcf47
Harshit Singla <harshit.singla@airtel.com>
2024-02-10 13:52:54 +0530

    Adding a way to add child using createElement

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement(
   "h1",
   //used to give attributes to a tag
@@ -6,6 +8,16 @@ const heading = React.createElement(
   },
   "Hello World from react"
 );
+
+// Way of rendering Child
+// const element = React.createElement(
+//   "div",
+//   //used to give attributes to a tag
+//   {
+//     id: "child",
+//   },
+//   [React.createElement("h1", {}, "I am h1 tag")]
+// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
